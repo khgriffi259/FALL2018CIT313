84a127121b0185fe3e3c6a20c6d62ee68377bfd0
Kyle <kyle.khgriffi@gmail.com>
2018-09-09 19:30:27 -0400

    Assignment A1 commit
    Assignment A1 commit

---
+++ a1/CIT_FALL_2018-master/Debug-Exercise-master/webconfig.php
@@ -0,0 +1,4 @@
+<?php
+
+define('ABSOLUTE_PATH', '/home/kyhgriff/htdocs/CIT313/FA2018/a1/CIT_FALL_2018-master/Debug-Exercise-master');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:23641/CIT313/FA2018/a1/CIT_FALL_2018-master/Debug-Exercise-master');

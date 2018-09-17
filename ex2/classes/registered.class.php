95cf1caef91da974758a5bca89b70740fb03c63d
Kyle <kyle.khgriffi@gmail.com>
2018-09-16 23:17:28 -0400

    ex2 initial commit
    ex2 object oriented programming introduction

---
+++ ex2/classes/registered.class.php
@@ -0,0 +1,23 @@
+<?php
+class Registered_User extends User
+{
+
+    public function __construct($user_level, $user_id)
+    {
+        parent::__construct($user_level);
+        $this->user_type = 1;
+        $this->user_id = $user_id;
+    }
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    public function __destruct()
+    {
+    }
+}
---
+++ ex2/index.php
@@ -0,0 +1,45 @@
+<?php 
+include_once("./classes/user.class.php");
+include_once("./classes/registered.class.php");
+include_once("./classes/admin.class.php");
+
+$registered_user = new Registered_User('Regular User', 'kyhgriff');
+$admin_user = new Admin_User('Administrator', 'lynnable');
+
+$registered_user->first_name = "Kyle";
+$registered_user->last_name = "Griffin";
+$registered_user->email_address = "kyhgriff@iu.edu";
+
+$admin_user->first_name = "Lynn";
+$admin_user->last_name = "Venable";
+$admin_user->email_address = "Lynnerd@iu.edu";
+
+
+echo 'User Level: ' . $registered_user->user_level . '<br/>';
+echo 'Registered User ID: ' . $registered_user->user_id . '<br/>';
+echo 'Registered User Type: ' . $registered_user->user_type . '<br/>';
+echo 'Registered First Name: ' . $registered_user->first_name . '<br/>';
+echo 'Registered Last Name: ' . $registered_user->last_name . '<br/>';
+echo 'Registered Email: ' . $registered_user->email_address . '<br/>';
+echo '<hr>';
+echo 'User Level: ' . $admin_user->user_level . '<br/>';
+echo 'Admin User ID: ' . $admin_user->user_id . '<br/>';
+echo 'Admin User Type: ' . $admin_user->user_type . '<br/>';
+echo 'Admin First Name: ' . $admin_user->first_name . '<br/>';
+echo 'Admin Last Name: ' . $admin_user->last_name . '<br/>';
+echo 'Admin Email: ' . $admin_user->email_address . '<br/>';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
